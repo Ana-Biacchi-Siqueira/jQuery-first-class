01e485c0c20a436ff6ee9f66ea68c291b1789f67
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-07-04 16:19:57 -0300

    Finished fourth class

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,31 +1,51 @@
-var phrase = $(".phrase").text();
-var numOfWords = phrase.split(" ").length;
-var phraseLength = $("#phraseLength");
-
-phraseLength.text(numOfWords);
-
+var refreshTime = $("#typingTimer").text();
 var area = $(".typingArea");
-area.on("input", function () {
-  var content = area.val();
-  var wordsAmount = content.split(/\S+/).length - 1;
-  $("#wordAmount").text(wordsAmount);
-  var lettersAmount = content.length;
-  $("#letterAmount").text(lettersAmount);
+
+$(function () {
+  refreshPhrase();
+  startsAmountCount();
+  startsTimer();
+  $("#refreshButton").click(restartGame);
 });
 
-var timeLeft = $("#typingTimer").text();
-area.one("focus", function () {
-  var timeStop = setInterval(function () {
-    timeLeft--;
-    $("#typingTimer").text(timeLeft);
-    if (timeLeft < 1) {
-      area.attr("disabled", true);
-      clearInterval(timeStop);
-    }
-  }, 1000);
-});
+function refreshPhrase() {
+  var phrase = $(".phrase").text();
+  var numOfWords = phrase.split(" ").length;
+  var phraseLength = $("#phraseLength");
+  phraseLength.text(numOfWords);
+}
 
-$("#refreshButton").click(function () {
-  area.attr("disabled", false);
-  
-});
+function startsAmountCount() {
+  area.on("input", function () {
+    var content = area.val();
+    var wordsAmount = content.split(/\S+/).length - 1;
+    $("#wordAmount").text(wordsAmount);
+    var lettersAmount = content.length;
+    $("#letterAmount").text(lettersAmount);
+  });
+}
+
+function startsTimer() {
+  var timeLeft = $("#typingTimer").text();
+  area.one("focus", function () {
+    var timeStop = setInterval(function () {
+      timeLeft--;
+      $("#typingTimer").text(timeLeft);
+      if (timeLeft < 1) {
+        area.attr("disabled", true);
+        clearInterval(timeStop);
+      }
+    }, 1000);
+  });
+}
+
+function restartGame() {
+  $("#refreshButton").click(function () {
+    area.attr("disabled", false);
+    area.val("");
+    $("#wordAmount").text("0");
+    $("#letterAmount").text("0");
+    $("#typingTimer").text(refreshTime);
+    startsTimer();
+  });
+}
