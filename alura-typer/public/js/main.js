46d9da7bf3bf43825f5c0295a820bdc9f9a50f93
Ana Siqueira <ana.siqueira@gpnet.com.br>
2022-07-06 19:37:24 -0300

    Middle_sixth_class

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -1,5 +1,95 @@
-var phrase = $(".phrase").text();
-var numOfWords = phrase.split(" ").length;
-var phraseLength = $("#phraseLength");
+var refreshTime = $("#typingTimer").text();
+var area = $(".typingArea");
 
-phraseLength.text(numOfWords);
+$(function () {
+  refreshPhrase();
+  startsAmountCount();
+  startsTimer();
+  correctionSync();
+  $("#refreshButton").click(restartGame);
+});
+
+function refreshPhrase() {
+  var phrase = $(".phrase").text();
+  var numOfWords = phrase.split(" ").length;
+  var phraseLength = $("#phraseLength");
+  phraseLength.text(numOfWords);
+}
+
+function startsAmountCount() {
+  area.on("input", function () {
+    var content = area.val();
+    var wordsAmount = content.split(/\S+/).length - 1;
+    $("#wordAmount").text(wordsAmount);
+    var lettersAmount = content.length;
+    $("#letterAmount").text(lettersAmount);
+  });
+}
+
+function startsTimer() {
+  var timeLeft = $("#typingTimer").text();
+  area.one("focus", function () {
+    $("#refreshButton").attr("disabled", true);
+    var timeStop = setInterval(function () {
+      timeLeft--;
+      $("#typingTimer").text(timeLeft);
+      if (timeLeft < 1) {        
+      clearInterval(timeStop);
+      gameOver();
+      }
+    }, 1000);
+  });
+}
+
+function gameOver(){
+
+  area.attr("disabled", true);
+  area.toggleClass("areaDeactivated");
+  $("#refreshButton").attr("disabled", false);
+  boardInsert();
+}
+
+function correctionSync(){
+
+  var phrase = $(".phrase").text();
+  area.on("input",function(){
+    var typed = area.val();
+    var typingSync = phrase.substr(0,typed.length);
+    if(typed == typingSync){
+      area.addClass("correctSync");
+      area.removeClass("incorrectSync");
+    }else{
+      area.addClass("incorrectSync");
+      area.removeClass("correctSync");
+    }
+  });
+
+}
+
+function boardInsert(){
+    var scoringBoard = $(".scoreBoard").find("tbody");
+    var playerName = "Anna";
+    var numOfWords = $("#wordAmount").text();
+    var boardRow = "<tr>"+
+                      "<td>"+ playerName + "</td>"+
+                      "<td>"+ numOfWords + " words" + "</td>"+
+                    "</tr>"
+    scoringBoard.prepend(boardRow);
+
+}
+
+
+
+function restartGame() {
+  $("#refreshButton").click(function () {
+    area.attr("disabled", false);
+    area.val("");
+    $("#wordAmount").text("0");
+    $("#letterAmount").text("0");
+    $("#typingTimer").text(refreshTime);
+    startsTimer();
+    area.toggleClass("areaDeactivated");
+    area.removeClass("correctSync");
+    area.removeClass("incorrectSync");
+  });
+}
